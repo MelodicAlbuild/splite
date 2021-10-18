45bc20504221c37842f7d0b6143a4dfb42d180db
MelodicAlbuild <ralexdrum@gmail.com>
2021-10-18 20:57:48 +0000

    Role System vFinal

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -56,7 +56,7 @@ module.exports = async (client, message) => {
       let user1 = await client.users.cache.get(message.channel.name);
       if (user1) {
         return user1.send(
-          `**From ${message.member.roles.highest.name}: ${message.author.tag} in ${message.guild.name}:**\n` +
+          `**From ${message.member.roles.highest.name} ${message.author.tag} in ${message.guild.name}:**\n` +
             message.content
         );
       } else {
