f8390d2e1faed614947c8e6ea57eb514d63bce1c
MelodicAlbuild <ralexdrum@gmail.com>
2021-10-27 20:40:31 -0500

    Fix File Writing

--- src/commands/commands/owner/test.js
+++ src/commands/commands/owner/test.js
@@ -1,60 +1,69 @@
-const Command = require('../../Command.js');
-const { MessageButton, MessageActionRow, MessageAttachment } = require("discord.js");
+const Command = require("../../Command.js");
+const {
+  MessageButton,
+  MessageActionRow,
+  MessageAttachment,
+} = require("discord.js");
 
 module.exports = class TestCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'test',
-            usage: 'test',
-            description: `Functioning Test Command`,
-            type: client.types.OWNER,
-            ownerOnly: true
-        });
-    }
-    async run(message, args) {
-        const allButtons = [];
-        const button = new MessageButton()
-            .setCustomId(`Test`)
-            .setLabel(`Test`)
-            .setStyle("PRIMARY");
+  constructor(client) {
+    super(client, {
+      name: "test",
+      usage: "test",
+      description: `Functioning Test Command`,
+      type: client.types.OWNER,
+      ownerOnly: true,
+    });
+  }
+  async run(message, args) {
+    const allButtons = [];
+    const button = new MessageButton()
+      .setCustomId(`Test`)
+      .setLabel(`Test`)
+      .setStyle("PRIMARY");
 
-        allButtons.push(button);
+    allButtons.push(button);
 
-        let rows = new Array(allButtons.length)
-          .fill()
-          .map((r) => {
-            const row = new MessageActionRow();
-            const buttons = allButtons[0];
-            row.addComponents(buttons);
-            return row;
-          });
-        let msg = await message.channel.send({
-          components: rows,
-          content: "Help"
-        });
+    let rows = new Array(allButtons.length).fill().map((r) => {
+      const row = new MessageActionRow();
+      const buttons = allButtons[0];
+      row.addComponents(buttons);
+      return row;
+    });
+    let msg = await message.channel.send({
+      components: rows,
+      content: "Help",
+    });
 
-        const filter = (i) => {
-          i.deferUpdate();
-          return true;
-        };
+    const filter = (i) => {
+      i.deferUpdate();
+      return true;
+    };
 
-        msg
-          .awaitMessageComponent({
-            filter,
-            componentType: "BUTTON"
-          })
-          .then((interaction) => {
-            var fs = require("fs");
-            fs.writeFile("./exports.json", interaction, "utf8");
-            var attachments = new MessageAttachment();
-            attachments.setFile("./exports.json");
+    msg
+      .awaitMessageComponent({
+        filter,
+        componentType: "BUTTON",
+      })
+      .then((interaction) => {
+        var fs = require("fs");
+        fs.writeFile(
+          "./exports.json",
+          interaction,
+          "utf8",
+          function readFileCallback(err, data) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+        var attachments = new MessageAttachment();
+        attachments.setFile("./exports.json");
 
-            interaction.channel.send({files: [attachments]});
+        interaction.channel.send({ files: [attachments] });
 
-            interaction.editReply(
-              `You selected ${interaction.values.join(", ")}!`
-            );
-          })
-          .catch((err) => console.log(err));
-    }
+        interaction.editReply(`You selected ${interaction.values.join(", ")}!`);
+      })
+      .catch((err) => console.log(err));
+  }
 };
