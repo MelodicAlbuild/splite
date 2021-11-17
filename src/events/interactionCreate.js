5858995231d073c7dcd1b1877e89a8fc7923c514
MelodicAlbuild <ralexdrum@gmail.com>
2021-11-17 21:16:23 +0000

    Yoink

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -117,8 +117,8 @@ module.exports = async (client, interaction) => {
             interaction.member.send({ embeds: [masterEmbed] });
           }
         })
-
-        client.channels.get('910639165392175114').send('Hello here!');
+        
+        client.channels.cache.get('910639165392175114').send('Hello here!')
 
         interaction.deferUpdate();
 
