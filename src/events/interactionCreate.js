fd857640808bad91b7945b3163e3282f4ce902ca
MelodicAlbuild <ralexdrum@gmail.com>
2021-11-17 19:45:50 -0600

    To String

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -137,8 +137,8 @@ module.exports = async (client, interaction) => {
             "MelodicAlbuild",
             "https://cdn.discordapp.com/avatars/392502749876584448/7d3ab8457b9509dc783f447c4a77da55.webp?size=80"
           )
-          .addField("Minecraft", maddenValue, true)
-          .addField("Madden", minecraftValue, true);
+          .addField("Minecraft", maddenValue.toString(), true)
+          .addField("Madden", minecraftValue.toString(), true);
 
         client.channels.cache.get('910639165392175114').messages.fetch('910639655962173471').then(msg => msg.edit({ embeds: [numberEmbed] }))
 
