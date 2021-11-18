dc64d9a06163194ef35759a5766fd172db4f4af9
MelodicAlbuild <ralexdrum@gmail.com>
2021-11-17 20:13:08 -0600

    Add in Halo Infinite

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -144,9 +144,9 @@ module.exports = async (client, interaction) => {
                 "MelodicAlbuild",
                 "https://cdn.discordapp.com/avatars/392502749876584448/7d3ab8457b9509dc783f447c4a77da55.webp?size=80"
               )
-              .addField("Minecraft", "4", true)
+              .addField("Minecraft", maddenValue.toString(), true)
               .addField("Madden", minecraftValue.toString(), true)
-              .addField("Halo Infinite", "0", true);
+              .addField("Halo Infinite", haloValue.toString(), true);
 
               // .addField("Minecraft", maddenValue.toString(), true)
               // .addField("Madden", minecraftValue.toString(), true)
